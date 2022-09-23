fcbf2449553c82a71bcab6f1d2592754358aebc1
TheFluffyNebula <yinj@guilford.edu>
2022-09-23 13:24:26 -0400

    trying CP again

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import Toastify from 'toastify-js'
-btn = document.querySelector('#toaster');
-
-function toastIt(){
-    Toastify({
-        text: "This is a toast",
-        duration: 3000}).showToast();
-}
-btn.addEventListener('click',toastIt);
